29d7f843521af533232ccf03f1c9584203a7ae91
Carlos AAF <ca34922@gmail.com>
2026-03-17 09:47:09 -0400

    Refactor signal visualization to separate individual signals and improve clarity

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -2,10 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 t = np.linspace(0,1,1000)
-
-# señal compuesta
 x = np.sin(2*np.pi*5*t) + np.sin(2*np.pi*10*t)
 
+x1 = np.sin(2*np.pi*5*t)
+x2= np.sin(2*np.pi*10*t)
+
+plt.subplot(3,1,1)
+plt.plot(t,x1)
+plt.title("Señal 1 (5 Hz)")
+
+
+plt.subplot(3,1,2)
+plt.plot(t,x2)
+plt.title("Señal 2 (10 Hz)")
+
+plt.subplot(3,1,3)
 plt.plot(t,x)
 plt.title("Señal compuesta")
 plt.show()
+
